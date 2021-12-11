026d221fc3474f0e1a58cac3ae190f621baf3e74
RT401 <45774452+RT401@users.noreply.github.com>
2021-12-11 13:15:23 +1030

    Built timer
    Added timer
    added function to update and display timer through text

--- MK_Project/Assets/Scripts/Timer.cs
+++ MK_Project/Assets/Scripts/Timer.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    // make timer appear & run
+    public bool displayTimer = false;
+    public bool timerRunning = false;
+
+    // Timer values
+    public float maxTimer = 60;
+    public float currentTimer;
+
+    // Timer spawn location
+    public Text displayedTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTimer = maxTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (displayTimer == true)
+            DisplayTimer();
+
+        if (timerRunning == true)
+            currentTimer -= Time.deltaTime;
+    }
+
+    public void DisplayTimer()
+    {
+        displayedTimer.text = currentTimer.ToString("ss");
     }
 }
