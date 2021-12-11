a260e68b63adf39bf0795014d06701be14246236
RT401 <45774452+RT401@users.noreply.github.com>
2021-12-11 14:34:14 +1030

    Moved loading level into respective script
    Added loadLevel script
    Moved load level functions into loadLevel script

---
+++ MK_Project/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    ///  Levels
+    public GameObject Startmenu;
+    public GameObject Easy;
+    public GameObject Normal;
+    public GameObject Hard;
+    public GameObject CurrentLevel;
+
+    private void Start()
+    {
+        if (Startmenu == null)
+        {
+            GameObject[] listOfObjects = FindObjectsOfType<GameObject>();
+
+            foreach(GameObject go in listOfObjects)
+            {
+                if (go.name == "Startmenu")
+                    Startmenu = go;
+            }
+        }
+
+        if (Easy == null)
+        {
+            GameObject[] listOfObjects = FindObjectsOfType<GameObject>();
+
+            foreach (GameObject go in listOfObjects)
+            {
+                if (go.name == "Easy")
+                    Easy = go;
+            }
+        }
+
+        if (Normal == null)
+        {
+            GameObject[] listOfObjects = FindObjectsOfType<GameObject>();
+
+            foreach (GameObject go in listOfObjects)
+            {
+                if (go.name == "Normal")
+                    Normal = go;
+            }
+        }
+
+        if (Hard == null)
+        {
+            GameObject[] listOfObjects = FindObjectsOfType<GameObject>();
+
+            foreach (GameObject go in listOfObjects)
+            {
+                if (go.name == "Hard")
+                    Hard = go;
+            }
+        }
+    }
+
+    /// Setup to find and start chosen level
+    public void FindLevel(string chosenLevel)
+    {
+        if (chosenLevel == Easy.name)
+            Startlevel(Easy);
+
+        if (chosenLevel == Normal.name)
+            Startlevel(Normal);
+
+        if (chosenLevel == Hard.name)
+            Startlevel(Hard);
+    }
+
+
+    void Startlevel(GameObject Level)
+    {
+        /// Turns off start menu
+        /// turns on whatever level was selected
+        /// saves current level for later to turn off
+        Startmenu.SetActive(false);
+        Level.SetActive(true);
+        CurrentLevel = Level;
+    }
+
+    void EndLevel()
+    {
+        /// turns off current level
+        /// turns on start menu
+        CurrentLevel.SetActive(false);
+        Startmenu.SetActive(true);
+    }
+}
--- MK_Project/Assets/Scripts/Spawner.cs
+++ MK_Project/Assets/Scripts/Spawner.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
     public GameObject answerPrefab;
-    public GameObject Startmenu;
 
     enum colours
     {
@@ -20,32 +18,4 @@ public class Spawner : MonoBehaviour
         Purple,
         Green
     }
-
-    void FindLevel(string chosenLevel)
-    {
-        //search scene to find gameobject with enum chosens name
-        //then set that object as active and disable main menu object
-
-        Canvas[] allCanvasArray;
-        Canvas LevelCanvas;
-
-        allCanvasArray = FindObjectsOfType<Canvas>();
-
-        foreach(Canvas cv in allCanvasArray)
-        {
-            if (cv.gameObject.name == chosenLevel)
-                LevelCanvas = cv;
-        }
-
-        //Ensure that the level was found before try to start playing
-        if (LevelCanvas != null)
-            Startlevel(LevelCanvas);
-    }
-
-    void Startlevel(Canvas Level)
-    {
-        Startmenu.SetActive(false);
-        Level.gameObject.SetActive(true);
-    }
-
 }
